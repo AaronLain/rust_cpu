a57453138052993e05c4edc9b82c43a9c33de083
user <aaron.joel.lain@gmail.com>
2022-01-03 14:21:43 -0600

    spelling

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn run(&mut self) {
     let c = ((opcode & 0xF000) >> 12) as u8;
     let x = ((opcode & 0x0F00) >> 8) as u8;
     let y = ((opcode & 0x00F0) >> 4) as u8;
-    let d = ((opcdoe & 0x000F) >> 0) as u8;
+    let d = ((opcode & 0x000F) >> 0) as u8;
 
     match (c, x, y, d) {
         (0x8, _, _, 0x4) => self.add_xy(x, y),
